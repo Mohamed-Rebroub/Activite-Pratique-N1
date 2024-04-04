226fe0a30df65060879b41b24e4990fd2429caa8
Mohamed REBROUB <srebroub550@gmail.com>
2024-04-04 10:21:14 +0000

    first commit

---
+++ src/main/java/org/example/activitepratique/ActivitePratiqueN1Application.java
@@ -0,0 +1,13 @@
+package org.example.activitepratique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitePratiqueN1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitePratiqueN1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/activitepratique/ActivitePratiqueN1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.activitepratique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitePratiqueN1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
